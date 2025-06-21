46551b0e0e374f1e89907e45037aed8b4f718358
ramdom123046311 <123046311@upq.edu.mx>
2025-06-21 15:25:35 -0600

    Actualizacion de dashboard

--- MedicalCenter/app.py
+++ MedicalCenter/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
