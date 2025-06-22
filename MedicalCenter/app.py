d82f66dd5829399ea21c55a4e6aedaffabd9dce5
Samael Sánchez <antonio.sanchez35@hotmail.com>
2025-06-22 00:47:53 -0600

    Mapeo de rutas

--- MedicalCenter/app.py
+++ MedicalCenter/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/medicos')
+def medicos():
+    return render_template('medicos.html')
+
+@app.route('/pacientes')
+def pacientes():
+    return render_template('pacientes.html')
+
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
